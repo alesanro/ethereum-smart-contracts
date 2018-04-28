dd54b1c0f0d328e9670d0c8caeb8eec132036809
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-04-28 16:09:00 +0300

    Fix index.js; add example script

---
+++ examples/example1.js
@@ -0,0 +1,32 @@
+const SmartContracts = require("../index")
+
+module.exports = async function () {
+	const uri = "https://mainnet.infura.io/"
+	const options = {
+		from: "0x4a2d3fc1587494ca2ca9cdeb457cd94be5d96a61",
+		gas: 3000000,
+		gasPrice: 20
+	}
+	
+	const tokenAddresses = [
+		"0x7b39940dbac110f1227d37c395675def270afcd7", // EOSG
+		"0x6531f133e6deebe7f2dce5a0441aa7ef330b4e53", // TIME
+	]
+
+	const context = SmartContracts(uri)
+	const web3 = context.web3
+	
+	const tokens = []
+	for (var tokenAddress of tokenAddresses) {
+		const token = SmartContracts.getERC20TokenAt(web3, tokenAddress, options)
+		tokens.push(token)
+		console.log(`
+		##################
+		## Token '${await token.methods.symbol().call()}':
+		## - name: ${await token.methods.name().call()}
+		## - totalSupply: ${await token.methods.totalSupply().call()}
+		## - decimals: ${await token.methods.decimals().call()}
+		`)
+	}
+}
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ERC20Abi = require("./build/contracts/ERC20")
+const ERC20Artifact = require("./build/contracts/ERC20")
 const Web3 = require("web3")
 
 const getContractAt = (web3, contractAbi, address, { from, gas, gasPrice, }) => {
@@ -11,18 +11,19 @@ const getContractAt = (web3, contractAbi, address, { from, gas, gasPrice, }) => 
 	return contract
 }
 
-module.exports = (uri, { account, gas, }) => {
-	// const web3 = new Web3(new Web3.providers.HttpProvider(uri))
+module.exports = (uri) => {
+	const web3 = new Web3(new Web3.providers.HttpProvider(uri))
 
 	return {
 		contracts: {
-			ERC20: ERC20Abi,
-		}
+			ERC20: ERC20Artifact,
+		},
+		web3: web3,
 	}
 
 }
 
 module.exports.getContractAt = getContractAt
 module.exports.getERC20TokenAt = (web3, address, options) => {
-	return getContractAt(web, ERC20Abi, address, options)
+	return getContractAt(web3, ERC20Artifact.abi, address, options)
 }
