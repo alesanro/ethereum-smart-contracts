4ab4cb3b16e2c80333ab86a84b17b9a92dc22a00
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-05-24 20:49:35 +0300

    Setup truffle; update package.json

--- truffle.js
+++ truffle.js
@@ -1,4 +1,45 @@
+var HDWalletProvider = require("truffle-hdwallet-provider") /* eslint global-require: 0 */
+
+function getWallet() {
+	try {
+		return require('fs').readFileSync("./wallet.json", "utf8").trim()
+	}
+	catch (err) {
+		return ""
+	}
+}
+
+function getPassword() {
+	try {
+		return JSON.parse(require('fs').readFileSync("./secret.json", "utf8").trim())["password"]
+	}
+	catch (err) {
+		return ""
+	}
+}
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+	// See <http://truffleframework.com/docs/advanced/configuration>
+	// to customize your Truffle configuration!
+
+	networks: {
+		"private": {
+			provider: new HDWalletProvider.WalletProvider(getWallet(), getPassword(), "http://127.0.0.1:8540"),
+			network_id: 74565,
+			gasPrice: 0,
+			gas: 4700000,
+		},
+		test: {
+			host: "localhost",
+			port: "8545",
+			network_id: "*",
+			gas: 4700000,
+		},
+	},
+	solc: {
+		optimizer: {
+			enabled: true,
+			runs: 200,
+		},
+	},
+}
